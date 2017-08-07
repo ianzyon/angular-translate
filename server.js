7570ba5ec6fbf45c8ff81946f20b95e810e786dd
Ian Vieira <iansalgado@yahoo.com.br>
2017-08-07 00:01:33 -0300

    full directories

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
 
 // listen gera um server de http na porta informada
 app.listen(port,
@@ -6,3 +8,8 @@ app.listen(port,
         console.log(`Server is up on port ${port}`);
     }
 );
+
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
